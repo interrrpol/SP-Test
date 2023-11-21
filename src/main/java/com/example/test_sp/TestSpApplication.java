1e434c0dac5d6241d0a205519a3b92b07287b51f
interrrpol <gia.ljuba02@e-uvt.ro>
2023-11-21 08:49:12 +0200

    test

--- src/main/java/com/example/test_sp/TestSpApplication.java
+++ src/main/java/com/example/test_sp/TestSpApplication.java
@@ -2,12 +2,23 @@ package com.example.test_sp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 @SpringBootApplication
 public class TestSpApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TestSpApplication.class, args);
-    }
-
+    SpringApplication.run(TestSpApplication.class, args);
+    ObjectMapper objectMapper = new ObjectMapper();
+    String filePath = "src/main/java/com/example/test_sp/messages.json";
 }
